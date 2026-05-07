47715ba71b975f7ef449e86afa8b6bef3a3d4863
theBF <brian.fox@brianfox.ca>
2026-05-06 20:33:37 -0400

    benchmark re-testing

--- Benchmarks/UltimateBenchmarks/TAKEUCHI/rabithat/Takeuchi_v007.fs
+++ Benchmarks/UltimateBenchmarks/TAKEUCHI/rabithat/Takeuchi_v007.fs
@@ -1,4 +1,4 @@
-\ Takeuchi function benchmark with CAMEL99 Optimizations 
+\ Takeuchi function benchmark with CAMEL99 Optimizations
 \ ----------------------------------------------------------------------------------------
 \
 \ Lisp definition of Takeuchi function (Copied from: http://www.ulisp.com/show?1EO1):
@@ -9,14 +9,14 @@
 \      (tak (1- x) y z)
 \      (tak (1- y) z x)
 \      (tak (1- z) x y))))
-	 
+
 \ ----------------------------------------------------------------------------------------
 \ Equivalent Forth definition of Takeuchi function:
 
-\ Edited for Camel99 Forth 
-NEEDS ELAPSE FROM DSK1.ELAPSE  \ timer 
+\ Edited for Camel99 Forth
+NEEDS ELAPSE FROM DSK1.ELAPSE  \ timer
 
-\ Stuff not in Camel99 kernel 
+\ Stuff not in Camel99 kernel
 : ROLL \ nn..n0 n -- nn-1..n0 nn ; 6.2.2150
   DUP>R PICK
   SP@ DUP CELL+ R> 1+ CELLS MOVE DROP ;
@@ -24,29 +24,29 @@ NEEDS ELAPSE FROM DSK1.ELAPSE  \ timer
 : >=   S" 1- >" EVALUATE ; IMMEDIATE
 
 \ ------------------------------------------------
-INCLUDE DSK1.3RD4TH 
-INCLUDE DSK1.TAILCALL 
+INCLUDE DSK1.3RD4TH
+INCLUDE DSK1.TAILCALL
 \ ( x y z -- x y z boolean)
 : NOT_Y<X?   OVER 4TH >= ;
 
 \ ( x y z -- z )
 : ONLY_Z     NIP NIP ;
 
-\ ( x y z -- x y z x-1 y z)	 
+\ ( x y z -- x y z x-1 y z)
 : TAKX   3RD 1-  3RD 3RD ;
 
 \ ( x y z result1 -- x y z result1 y-1 z x)
 : TAKY  3RD 1-  3RD 5 PICK ;
 
 \ ( x y z result1 result2 -- result1 result2 (z-1) x y)
-: TAKZ   2 ROLL 1-  4 ROLL 4 ROLL -; 
+: TAKZ   2 ROLL 1-  4 ROLL 4 ROLL -;
 
 \ ( x y z -- result )
-: TAK NOT_Y<X? 
-   IF ONLY_Z 
-   ELSE TAKX RECURSE 
-        TAKY RECURSE 
-        TAKZ RECURSE RECURSE 
+: TAK NOT_Y<X?
+   IF ONLY_Z
+   ELSE TAKX RECURSE
+        TAKY RECURSE
+        TAKZ RECURSE RECURSE
    THEN ;
 
-\ 18 12 6 TAK   are the benchmark arguments 
+: GO   18 12 6 TAK ;   are the benchmark arguments
